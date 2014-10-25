31b80ca73cac71692dfc0d1d960d6b789c010ec1
Colin B Hamilton <cbhsixtysix@gmail.com>
2014-10-25 02:02:02 -0400

    Add python scripts to find overlaps between students

---
+++ overlaps.py
@@ -0,0 +1,62 @@
+from intervaltree import *
+
+
+def amount_overlap(start1, end1, start2, end2):
+    later_start = start1 if start1 > start2 else start2
+    earlier_end = end1 if end1 < end2 else end2
+    time = earlier_end - later_start if earlier_end > later_start else 0
+    return (time, later_start, earlier_end)
+
+
+def list_overlaps(tree, start, end):
+    result = {}
+    for x in tree[start:end]:
+        if x.data not in result:
+            result[x.data] = []
+        result[x.data].append(amount_overlap(x.begin, x.end, start, end))
+    return result
+
+
+def concatenate_list_dicts(d1, d2):
+    result = {}
+    for key in d1:
+        result[key] = d1[key]
+    for key in d2:
+        if key not in result:
+            result[key] = []
+        result[key].append(d2[key])
+    return result
+
+def clean(d):
+    for key in d:
+        one_list = []
+        for elem in d[key]:
+            for tup in elem:
+                one_list.append(tup)
+        d[key] = one_list
+    return d
+
+
+def student_overlaps(tree, student_name):
+    student_times = []
+    totals = {}
+    for period in tree:
+        if period.data == student_name:
+            totals = concatenate_list_dicts(totals, list_overlaps(tree, period.begin, period.end))
+    return clean(totals)
+
+
+def best_partner(tree, student_name):
+    laps = student_overlaps(tree, student_name)
+    total_time = {}
+    time_squares = {}
+    for partner in laps:
+        total_time[partner] = time_squares[partner] = 0
+        for time in laps[partner]:
+            (inc, _, _) = time
+            total_time[partner] += inc;
+            time_squares[partner] += inc ** 2
+    print total_time
+    print time_squares
+
+
