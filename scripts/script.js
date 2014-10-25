352bf2032a8be48071a68babcd6fab10c57ccd28
Charlie Meyer <charles.meyer@tufts.edu>
2014-10-25 09:46:03 -0400

    charlie is the post office

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ $('#submitlogin').click(function() {
         name: $('#studentname').val(),
         email: $('#studentemail').val(),
     }
-    localStorage["email"] = post_params["email"];
+    localStorage.setItem("gphemail", $('#studentemail').val());
     $.post('/adduser', post_params);
     window.location.href = "prefs.html";
 });
@@ -26,14 +26,25 @@ $('#submit40').click(function() {
 });
 
 $('#submitprefs').click(function() {
+	console.log("YO")
     post_params = {
-    	email: localStorage["email"],
-        free_times: $("#free_times").val(),
+    	email: localStorage.getItem("gphemail"),
+        free_times: $("#freeTimes").val(),
+    	timeofday: $("input[name=timeofday]:checked").val(),
+    	whotypes: $("input[name=whotypes]:checked").val(),
+    	texteditor: $("input[name=texteditor]:checked").val(),
+    	classyear: $("input[name=classyear]:checked").val(),
     };
+    console.log(post_params);
     function success(data, textStatus, jqXHR) {
         console.log(data);
     }
+    window.location.assign('success.html');
     $.post('/addprefs', post_params, success);
+});
+
+$('#sendmatches').click(function() {
+	$.post('/dothematches', "yo just give me a fucking cron job ;)");
 });
 
 //var OauthUrl = "https://accounts.google.com/o/oauth2/auth?scope=https://www.googleapis.com/auth/calendar.readonly&redirect_uri=http://group-40.appspot.com/oauth2callback&state=%2Fprofile&response_type=token&client_id=887830103143-ptqtmjls6qvpgjdiv94h5g4oogd0230i.apps.googleusercontent.com";
